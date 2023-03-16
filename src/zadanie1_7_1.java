3121df6f3e21a5ae57b1e58f3694c80c501601fa
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-16 12:16:19 +0300

    Initial commit

---
+++ src/zadanie1_7_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class zadanie1_7_1 {
+    //1. Написать простой булев калькулятор.
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter boolean expression: ");
+        String expr = scanner.nextLine();
+        char x = expr.charAt(0);
+        char op = expr.charAt(1);
+        char y = expr.charAt(2);
+
+        boolean xValue = false;
+        if (x == '1') {
+            xValue = true;
+        } else {
+            xValue = false;
+        }
+        boolean yValue = false;
+        if (y == '1') {
+            yValue = true;
+        } else {
+            yValue = false;
+        }
+        if (op == '&') {
+            System.out.println(xValue && yValue);
+        } else if (op == '|') {
+            System.out.println(xValue || yValue);
+        } else {
+            System.out.println("Unknown operator");
+        }
+        System.out.println("Ответ на задание 1: Пример правильных подстановок см. в коде.");
+    }
+}
